29e35b98748b9110f17d302046c5b40b338d2a13
oleskiy OS <12lie.to.me04@gmail.com>
2020-07-08 00:09:19 +0200

    adding config for heroku

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+  "host" => "mysql:host=sql7.freesqldatabase.com",
+    "dbname" => "sql7352662",
+    "username" => "sql7352662",
+    "password" => "kZXUNS1psg",
+    "options" => [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]
+];
