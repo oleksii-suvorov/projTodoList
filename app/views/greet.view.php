6ed70a1e42dcec9257aff9508acee69f9887d028
oleskiy OS <12lie.to.me04@gmail.com>
2020-07-08 00:22:47 +0200

    path error chech

--- app/views/greet.view.php
+++ app/views/greet.view.php
@@ -4,7 +4,7 @@
         <h1>Greetings from Oleskiy's Todos App</h1>
     </header>
     <div class="btn">
-        <a href="todos">Get Started</a>
+        <a href="oleskiys-todo.herokuapp.com/todos">Get Started</a>
     </div>
 
 <?php include "partials/footer.php" ?>
