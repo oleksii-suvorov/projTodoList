beddbc41933cfc86a250f204b522595b4371f52a
Oleksii <56698837+oleksii-suvorov@users.noreply.github.com>
2020-07-10 00:08:01 +0200

    upd

--- autorun.sh
+++ autorun.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
-
 sudo chmod +x ./SQLTableCreate.sh;
 ./SQLTableCreate.sh;
-composer install;
+echo -e "Insert your lacal DATABASE password: \c";
+read PWD;
+sed -i "/MYSQLDB_USERNAME=/c\MYSQLDB_USERNAME=$USER" ./.env;
+sed -i "/MYSQLDB_PWD=/c\MYSQLDB_PWD=$PWD" ./.env;
+~/composer/composer.phar install;
+RESULT=$?
+
+if [ $RESULT -eq 0 ]; then
+  echo success
+else
+  echo -e "Sorry, did not find composer, please, type full path to 'composer.phar' file (/home/USER/composer/composer.phar): \c";
+  read COMPOSER_PATH;
+  $COMPOSER_PATH install;
+fi
+
+echo "configuring composer...";
+sleep 1;
+echo "running server...";
+
 php -S localhost:8080;
 
 exit 0
